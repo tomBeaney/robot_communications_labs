01ec230646e807b9b9468163be3431def92da8f7
tom <tb21aai@herts.ac.uk>
2024-03-03 23:06:48 +0000

    made the code work

--- main.cpp
+++ main.cpp
@@ -7,9 +7,8 @@
 #include <chrono>
 #include <cstdint>
 #include <cstdio>
-//#include <cstdint>
-//#include <cstring>
-//#include <string>
+#include <cstring>
+#include <string>
 
 
 
@@ -17,106 +16,84 @@
 #define BLINKING_RATE     10ms
 
 #define BUFF_LENGTH       64
-#define MSG_BUFF_LENGTH 32
+#define MSG_BUFF_LENGTH 64
 
 int length;
-char buffer[BUFF_LENGTH] = {};
+char buffer[BUFF_LENGTH] = {0};
 
 UnbufferedSerial pc(USBTX, USBRX, 115200);
-char buf[32];
-char MSG[32];
-char rec[32];
-
+char buf[64];
+char MSG[64];
+char rec[64];
+int rec_pointer = 0;
 //BufferedSerial pc(USBTX, USBRX, 115200);
 
 //BufferedSerial pc(PA_11, PA_12, 115200);
 
-// use com 5 for conenction in PUTTY
 int  number = 0;
 DigitalOut led(LED1);
 //DigitalOut led1(D2);
-const char* test_msg = "{LED:status}";
+const char* test_msg = "{LED:status}"; //test messages to check if string manipulation works
 const char* tes_msg2 = "{LED:change}";
+int commandNumber = 0;
 
 //TO be used with UWP lab_1
-//Thread mythread;
-/*
-void threadMethod()
-{
-    // Initialise the digital pin LED1 as an output
-    //DigitalOut led(LED1);
-    while(true)
-    {
-        if(buf = pc.read(rec,sizeof(rec))){
-            if (strcmp(rec, "{change:\"LED\"}")==0){
-                led = !led; //turn the led to opposite state that it is in
-                length = snprintf(MSG,MSG_BUFF_LENGTH,"Received Command LED1\r\n");
-                pc.write(MSG,length);
-                
-            }else if (strcmp(rec, "{get_status:\"LED\"}")==0) {
-                length = snprintf(MSG,MSG_BUFF_LENGTH,"{status:{LED1:%d}}\r\n",int(led));
-                pc.write(MSG,length);
-            } 
-        }
-        ThisThread::sleep_for(BLINKING_RATE); //sleep the thread for 500ms
-    }
-}
-*/
+
 int main()
 {
     length = snprintf(buffer,BUFF_LENGTH,"\r\nProgram Starting\r\n");
     pc.write(buffer,length); //write the message to the serial port
-    //mythread.start(threadMethod); //start running the thread 
-    char msg_command[4]= {test_msg[1], test_msg[2] , test_msg[3]};
-    char msg_target[7] = {test_msg[5],test_msg[6],test_msg[7],test_msg[8],test_msg[9],test_msg[10]};
-    length = snprintf(buffer, BUFF_LENGTH, "%s \n\r",msg_command);
-    pc.write(buffer, length);
-    length = snprintf(buffer, BUFF_LENGTH, "%s \n\r",msg_target);
-    pc.write(buffer, length);
 
-    
+    //pc.set_flow_control(mbed::SerialBase::RTSCTS); results in error message code 0x80FF0144 mbedOS fatal error
 
     while (true) {
-
-        //length = snprintf(buffer,BUFF_LENGTH,"{Data:{Status:Correct %i}}",number); //create the message using string literal ''
-
-        //length = snprintf(buffer,BUFF_LENGTH,"{Data:{Status:\"Correct\"}}\r\n");
-        //pc.write(buffer,length); //write the message to the serial port
-        //ThisThread::sleep_for(chrono::seconds(1)); //sleep for 1 second on the serial thread using chrono library
-        //number --;
-
-        uint32_t num = pc.read(buf,sizeof(buf));
+        uint32_t num = pc.read(buf,sizeof(buf)); //reads only 1 character at the time so have to send messages as broken chars
+        length = snprintf(MSG,MSG_BUFF_LENGTH,"whats in the buffer: %s %d \n\r",buf,num);
+        pc.write(MSG,length);
         if(num > 0){
             //string msg = buf;
-            char msg_command[4]= {buf[1], buf[2] , buf[3]};
-            char msg_target[7] = {buf[5],buf[6],buf[7],buf[8],buf[9],buf[10]};
-            //string message_command = msg.substr(5,6);
-            //string message_target = msg.substr(1,3);
-            //const char* msg_command = message_command.c_str();
-            //const char* msg_target = message_target.c_str();
-            if (strcmp(msg_target, "LED")==0){
-                if (strcmp(msg_command, "change")==0){
-                    led = !led; //turn the led to opposite state that it is in
-                    length = snprintf(MSG,MSG_BUFF_LENGTH,"Received Command LED1\r\n");
-                    pc.write(MSG,length);
-                    
-                }else if (strcmp(msg_command, "status")==0) {
-                    length = snprintf(MSG,MSG_BUFF_LENGTH,"{status:{LED1:%d}}\r\n",int(led));
-                    pc.write(MSG,length);
-                } 
+            //builds own circular buffer that stores last chars received until terminator value is called (0)
+            rec[rec_pointer] = buf[0];
+            rec_pointer ++;
+            length = snprintf(MSG,MSG_BUFF_LENGTH,"rec : %s \n\r rec pointer: %d \n\r",rec,rec_pointer); //see what character we got and whats in the buffer
+            pc.write(MSG,length);
+            if (rec_pointer > sizeof(rec)-1){ //stop buffer overflow
+                rec_pointer = 0;
             }
-            if (strcmp(msg_target,"RNG")){
-
+            if (strcmp(buf, "0")==0){
+                length = snprintf(MSG,MSG_BUFF_LENGTH,"end of message\n\r");//create the message using string literal ''
+                pc.write(MSG,length);
+                char msg_command[4]= {rec[rec_pointer-12], rec[rec_pointer-11] , rec[rec_pointer-10]};
+                char msg_target[7] = {rec[rec_pointer-8],rec[rec_pointer-7],rec[rec_pointer-6],rec[rec_pointer-5],rec[rec_pointer-4],rec[rec_pointer-3]};
+                //string manipulation to read contense of message in format "{command:thing to do}"
+                if (strcmp(msg_command, "LED")==0){
+                    if (strcmp(msg_target, "change")==0){
+                        led = !led; //turn the led to opposite state that it is in
+                        length = snprintf(MSG,MSG_BUFF_LENGTH,"Received Command LED1\r\n");
+                        pc.write(MSG,length);
+                        commandNumber ++;
+                        
+                    }else if (strcmp(msg_target, "status")==0) {
+                        length = snprintf(MSG,MSG_BUFF_LENGTH,"{status:{LED1:%d}}\r\n",int(led));//return what state the LED is currently in 
+                        pc.write(MSG,length);
+                    } 
+                }
+                else if (strcmp(msg_command,"CMD")==0){
+                    if (strcmp(msg_target, "status")==0){
+                        length = snprintf(MSG,MSG_BUFF_LENGTH,"{status:{CMD:%d}}\r\n",commandNumber); //returns the number of times LED has been changed
+                        pc.write(MSG,length);
+                    }
+                }
+                else{//error checking the received message
+                    length = snprintf(MSG,MSG_BUFF_LENGTH,"whats the command: %s\n\r",msg_command);
+                    pc.write(MSG,length);
+                    length = snprintf(MSG,MSG_BUFF_LENGTH,"whats the target: %s\n\r",msg_target);
+                    pc.write(MSG,length);
+                }
+                rec_pointer = 0; //reset the buffer when we have got a successful message
             }
         }
-        ThisThread::sleep_for(BLINKING_RATE); //sleep the thread for 500ms
-        /*
-        length = snprintf(buffer,BUFF_LENGTH,"{Data:{Status:\"Correct\"}}\r\n");
-        pc.write(buffer,length); //write the message to the serial port
-        ThisThread::sleep_for(chrono::seconds(1)); //sleep for 1 second on the serial thread using chrono library
-        number --;
-        */
+        ThisThread::sleep_for(1ms); //sleep the thread for 1ms
 
     }
-    //mythread.join(); //join the thread to close it at the end of the program
 }
